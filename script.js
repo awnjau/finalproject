c1c63a97753a3c8716d38fba068e1067d80ea55e
Aisha Njau <aishanjau@10-23-0-239.dynapool.nyu.edu>
2022-05-03 00:33:57 -0400

    updated

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-Fancybox.bind('[data-fancybox="gallery"]', {
-  caption: function (fancybox, carousel, slide) {
-    return (
-      `${slide.index + 1} / ${carousel.slides.length} <br />` + slide.caption
-    );
-  },
-});
+
